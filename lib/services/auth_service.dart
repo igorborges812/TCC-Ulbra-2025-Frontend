4e77a8661bfdc1b20ca9b8109f76faac4a142fea
Igor Borges <61064081+igorborges812@users.noreply.github.com>
2025-07-04 02:21:44 -0300

    ajuste recuperar senha

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -9,6 +9,16 @@ class AuthService {
     ),
   );
 
+  final Dio _supabase = Dio(
+    BaseOptions(
+      baseUrl: 'https://sizovghaygzecxbgvqvb.supabase.co/auth/v1',
+      headers: {
+        'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpem92Z2hheWd6ZWN4Ymd2cXZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk2MDg2MTMsImV4cCI6MjA2NTE4NDYxM30.6etw0TwLyChIFDAIRWK0uhADrHNHn-qlYkFld9F5VVE',
+        'Content-Type': 'application/json',
+      },
+    ),
+  );
+
   Future<String?> getToken() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getString('token');
@@ -21,36 +31,46 @@ class AuthService {
 
   Future<String?> login(String email, String password) async {
     try {
-      final response = await _dio.post('/users/login/', data: {
+      final response = await _supabase.post('/token?grant_type=password', data: {
         'email': email,
         'password': password,
       });
 
-      final accessToken = response.data['access'];
+      final accessToken = response.data['access_token'];
 
       final prefs = await SharedPreferences.getInstance();
       await prefs.setString('token', accessToken);
 
-      // Atualiza o header global do Dio com o token para chamadas futuras
       _dio.options.headers['Authorization'] = 'Bearer $accessToken';
 
       return accessToken;
     } catch (e) {
-      print('Erro no login: $e');
+      print('Erro no login Supabase REST: $e');
       return null;
     }
   }
 
   Future<bool> register(String nickname, String email, String password) async {
     try {
-      await _dio.post('/users/register/', data: {
+      await _supabase.post('/signup', data: {
         'email': email,
         'password': password,
-        'nickname': nickname,
+        'data': {'nickname': nickname},
       });
+
       return true;
     } catch (e) {
-      print('Erro ao registrar: $e');
+      print('Erro ao registrar no Supabase REST: $e');
+      return false;
+    }
+  }
+
+  Future<bool> resetPassword(String email) async {
+    try {
+      await _supabase.post('/recover', data: {'email': email});
+      return true;
+    } catch (e) {
+      print('Erro ao enviar e-mail de recuperação: $e');
       return false;
     }
   }
